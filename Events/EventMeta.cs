f461514417de90466ecab256842471499051b4e0
Binpuki <30415792+Binpuki@users.noreply.github.com>
2024-11-22 14:52:28 -0600

    Format function to correct events just in case

--- Events/EventMeta.cs
+++ Events/EventMeta.cs
@@ -39,4 +39,32 @@ namespace Rubicon.Core.Events;
     /// </summary>
     /// <returns>True if there is a next event, false if not.</returns>
     public bool HasNext() => Index < Events.Length;
+
+    /// <summary>
+    /// Makes any corrections to this EventMeta that are necessary.
+    /// </summary>
+    public void Format()
+    {
+        Array.Sort(Events, (x, y) =>
+        {
+            if (x.Time < y.Time)
+                return -1;
+            if (x.Time > y.Time)
+                return 1;
+
+            return 0;
+        });
+    }
+    
+    /// <summary>
+    /// Gets the current index of this EventMeta.
+    /// </summary>
+    /// <returns>The current index</returns>
+    public int GetIndex() => Index;
+    
+    /// <summary>
+    /// Sets the current index.
+    /// </summary>
+    /// <param name="idx">The index to set</param>
+    public void SetIndex(int idx) => Index = idx;
 }

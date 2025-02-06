ac248dec7e862b3e4fb50423a2e335c3133f6b34
Binpuki <30415792+Binpuki@users.noreply.github.com>
2025-02-06 03:06:03 -0600

    GetAbsoluteFilePathsAt

--- PathUtility.cs
+++ PathUtility.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+
 namespace Rubicon.Core;
 
 public static class PathUtility
 {
+    public static string[] GetAbsoluteFilePathsAt(string path, bool recursive = false)
+    {
+        if (!DirAccess.DirExistsAbsolute(path))
+            return [];
+        
+        List<string> pathList = new();
+        if (recursive)
+        {
+            string[] directories = DirAccess.GetDirectoriesAt(path);
+            for (int i = 0; i < directories.Length; i++)
+                pathList.AddRange(GetAbsoluteFilePathsAt(directories[i], true));
+        }
+
+        string[] files = DirAccess.GetFilesAt(path);
+        for (int i = 0; i < files.Length; i++)
+            pathList.Add(path + files[i]);
+        return pathList.ToArray();
+    }
+    
     public static string GetResourcePath(string path) => GetPathWithExt(path, "res");
     
     public static string GetScenePath(string path) => GetPathWithExt(path, "scn");

15ee38227b8ddabe5559bf08e2c5e631cf5ac748
legole0 <legole0@proton.me>
2025-02-13 01:05:47 -0300

    Verbosity and print helpers

---
+++ PrintUtility.cs
@@ -0,0 +1,43 @@
+namespace Rubicon.Core;
+
+/// <summary>
+/// Print utilities.
+/// Lets you add an identifier to each print and determine
+/// if its a verbose print or not.
+/// </summary>
+[Tool] public static class PrintUtility
+{
+    private static readonly bool Verbose = ProjectSettings.GetSetting("rubicon/general/verbose").AsBool();
+
+    public static void Print(string message, bool onlyIfVerbose = false)
+    {   
+        if(!(Verbose == onlyIfVerbose || !onlyIfVerbose))
+            return;
+        
+        GD.Print(message);
+    }
+    
+    public static void Print(string identifier, string message, bool onlyIfVerbose = false)
+    {
+        if(!(Verbose == onlyIfVerbose || !onlyIfVerbose))
+            return;
+        
+        GD.Print($"[{identifier}] {message}");
+    }
+    
+    public static void PrintError(string message, bool onlyIfVerbose = false)
+    {   
+        if(!(Verbose == onlyIfVerbose || !onlyIfVerbose))
+            return;
+        
+        GD.PrintErr(message);
+    }
+    
+    public static void PrintError(string identifier, string message, bool onlyIfVerbose = false)
+    {
+        if(!(Verbose == onlyIfVerbose || !onlyIfVerbose))
+            return;
+        
+        GD.PrintErr($"[{identifier}] {message}");
+    }
+}
